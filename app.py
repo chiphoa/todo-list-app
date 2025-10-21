b4bcf0fcf84d86d68c6994285e938b9cca3e49f0
chiphoa <chauhanguyen2000@gmail.com>
2025-10-21 19:48:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # todo-list-app
 tasks = [] 
 
-def add_task(task_name): [cite: 56]
-    """Thêm một công việc mới vào danh sách.""" [cite: 57]
-    tasks.append(task_name) [cite: 57]
-    print(f"Đã thêm công việc: '{task_name}") [cite: 60]
+def add_task(task_name): 
+    """Thêm một công việc mới vào danh sách.""" 
+    tasks.append(task_name) 
+    print(f"Đã thêm công việc: '{task_name}") 
 
-if __name__ == "__main__": [cite: 62, 63, 64]
-    print("Chào mừng đến với ứng dụng To-Do List!") [cite: 65]
-    add_task("Học bài Git và GitHub") [cite: 66]
-    add_task("Làm bài tập thực hành ở nhà") [cite: 67]
+if __name__ == "__main__": 
+    print("Chào mừng đến với ứng dụng To-Do List!") 
+    add_task("Học bài Git và GitHub") 
+    add_task("Làm bài tập thực hành ở nhà") 
